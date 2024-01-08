d79b2ee176989f0d0cf8eead5e84359bbbe0b11f
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2024-01-08 11:55:45 +0000

    Get method

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
 from fastapi import FastAPI
+from enum import Enum
+from typing import Optional
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
 
 app = FastAPI()
 
 @app.get('/')
 def index():
     return { 'message' : 'Hello world!'}
+
+# @app.get('/blog/all')
+# def get_all_blogs():
+#     return { 'message': 'All blogs provided'}
+
+@app.get('/blog/all')
+def get_all_blogs(page = 1, page_size: Optional[int] = None):
+    return { 'message': f'All {page_size} blogs on page {page}'}
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return { 'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+@app.get('/blog/type/{type}')
+def get_blog_type(type: BlogType):
+    return { 'message': f'Blog type {type}' }
+
+@app.get('/blog/{id}')
+def index(id: int):
+    return { 'message' : f'Blog with id {id}'}
