ea377b2e81b2880b4ec3dc2bd0e1a34fd1105188
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2024-01-12 08:47:22 +0000

    Databases: Create database and user table

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from routers import blog_get, blog_post
+from db import models
+from db.database import engine
 
 app = FastAPI()
 app.include_router(blog_get.router)
@@ -8,3 +10,5 @@ app.include_router(blog_post.router)
 @app.get('/')
 def index():
     return { 'message' : 'Hello world!'}
+
+models.Base.metadata.create_all(engine)
