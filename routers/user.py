ed1b7e4d5566f1774a76343f7693536ed680893a
Bruno Rodrigo <bruno.rodrigo@definedcrowd.com>
2024-01-16 08:24:22 +0000

    Databases: Create and Read

--- routers/user.py
+++ routers/user.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from db.database import get_db
 from db import db_user
@@ -15,7 +16,15 @@ router = APIRouter(
 def create_user(request: UserBase, db: Session = Depends(get_db)):
     return db_user.creater_user(db, request)
 
-# Read user
+# Read all user
+@router.get('/', response_model=List[UserDisplay])
+def get_all_users(db: Session = Depends(get_db)):
+    return db_user.get_all_users(db)
+
+# Read one user
+@router.get('/{id}', response_model=UserDisplay)
+def get_user(id: int, db: Session = Depends(get_db)):
+    return db_user.get_user(db, id)
 
 # Update user
 
