0e7f9c3622770757bfbf4de5f02ed00d754df8c9
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-01-25 08:43:57 +0000

    Authentication - User authentication

--- routers/article.py
+++ routers/article.py
@@ -3,9 +3,9 @@ from fastapi import APIRouter, Depends
 from db.database import get_db
 from db import db_article
 
-from schemas import ArticleBase, ArticleDisplay
+from schemas import ArticleBase, ArticleDisplay, UserBase
 from sqlalchemy.orm import Session
-from auth.oauth2 import oauth2_scheme
+from auth.oauth2 import get_current_user, oauth2_scheme
 
 router = APIRouter(
     prefix='/article',
@@ -14,10 +14,21 @@ router = APIRouter(
 
 # Create article
 @router.post('/', response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+def create_article(
+    request: ArticleBase, 
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user)
+):
     return db_article.create_article(db, request)
 
 # Get specific article
-@router.get('/{id}', response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
-    return db_article.get_article(db, id)
+@router.get('/{id}')#, response_model=ArticleDisplay)
+def get_article(
+    id: int, 
+    db: Session = Depends(get_db), 
+    current_user: UserBase = Depends(get_current_user)
+):
+    return {
+        'data': db_article.get_article(db, id),
+        'current_user': current_user
+    }
