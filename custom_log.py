5ffbc64ae1abad37eb980bf40a1528e143c5c17a
Bruno Rodrigo <bnorodrigo@gmail.com>
2024-02-07 08:26:05 +0000

    Global Dependencies

--- custom_log.py
+++ custom_log.py
@@ -1,5 +1,6 @@
 import datetime
+from fastapi.requests import Request
 
-def log(tag="", message=""):
-    with open("log.txt", "a") as log:
-        log.write(f'[{datetime.datetime.now()}] {tag}: {message}\n')
+def log(tag="MyApp", message="", request: Request= None):
+    with open("log.txt", "a+") as log:
+        log.write(f'[{datetime.datetime.now()}] {tag}: {message} \t{request.url} \n')
